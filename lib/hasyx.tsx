ed0efdd3192c3e3fd91f60a0a3c5cb690a4ae0fb
Ivan S Glazunov <ivansglazunov@gmail.com>
2025-04-22 14:48:12 +0300

    remove css from lib

--- lib/hasyx.tsx
+++ lib/hasyx.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from "hasyx/components/theme-provider";
 import { useCreateApolloClient, useSession } from 'hasyx'; // Our client creation function
 import { SessionProvider } from "next-auth/react"; // Import SessionProvider and useSession
 import { useMemo } from "react";
-import "./globals.css";
 
 export function HasyxProvider({ children }: { children: React.ReactNode }) {
   const { data: session } = useSession(); // Get session
