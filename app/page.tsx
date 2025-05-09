d443c97230b160e1f950cc44a394c16d654d8cf4
Ivan S Glazunov <ivansglazunov@gmail.com>
2025-05-09 12:06:48 +0300

    Update version to 0.1.120 in package.json, refactor imports in page.tsx to use consistent paths, and add a new CSS class for table display in styles.css for improved layout handling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,11 @@
-import { Sidebar } from "hasyx/components/sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "hasyx/components/ui/breadcrumb"
-import { Separator } from "hasyx/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "hasyx/components/ui/sidebar"
 
-import pckg from "hasyx/package.json"
 
-import { AuthActionsCard } from "hasyx/components/auth/auth-actions-card"
-import { CredentialsSignInCard } from "hasyx/components/auth/credentials-signin-card"
-import { SessionCard } from "hasyx/components/auth/session-card"
-import { HasuraCard } from "hasyx/components/hasura/card"
-import { ProxyCard } from "hasyx/components/proxy/card"
-import { UsersCard } from "hasyx/components/users/users-card"
-import { NotificationCard } from "hasyx/components/notify"
 
 // Imports for getting server-side session
 import authOptions from "@/app/options"
 
 import sidebar from "@/app/sidebar"
-import Diagnostics from "@/components/diagnostics"
+import Diagnostics from "hasyx/components/diagnostics"
 import useSsr, { SsrResult } from "hasyx/lib/ssr"
 
 // Now this is an async server component
