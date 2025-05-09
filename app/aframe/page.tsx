b3ca7ccd53e845b3c9386d57495d8838356e95eb
Ivan S Glazunov <ivansglazunov@gmail.com>
2025-05-09 10:26:48 +0300

    Update version to 0.1.119 in package.json and remove activeUrl prop from Sidebar component in AframePage for cleaner code structure.

--- app/aframe/page.tsx
+++ app/aframe/page.tsx
@@ -37,7 +37,7 @@ export default function AframePage() {
   return (
     <SidebarProvider>
       {/* Make sure that activeUrl is correct */}
-      <Sidebar activeUrl={'/aframe'} data={sidebar} />
+      <Sidebar data={sidebar} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
