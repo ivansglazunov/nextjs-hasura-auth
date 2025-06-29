80828dfa057958a6edd3c376963d2b0d3d51d927
ivansglazunov <ivansglazunov@gmail.com>
2025-06-29 15:03:46 +0200

    fix: update import path for project schema in client.tsx - Changed the import path for the project schema from a relative path to a more appropriate location in the public directory, ensuring correct access to the schema file.

--- app/hasyx/cyto/client.tsx
+++ app/hasyx/cyto/client.tsx
@@ -5,7 +5,7 @@ import { Cyto, CytoStyle } from "hasyx/lib/cyto";
 import { Card as EntityCard, Button as EntityButton } from '../../../lib/entities';
 import { QueriesManager, QueriesRenderer } from 'hasyx/lib/renderer';
 import { useCallback, useMemo, useState } from "react";
-import projectSchema from '../hasura-schema.json';
+import projectSchema from '../../../public/hasura-schema.json';
 
 const debug = Debug('cyto');
 
